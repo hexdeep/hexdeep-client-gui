f3cb3d48d44fa395e751ff065e0fd710a2986300
Sauce233 <2892709432@qq.com>
2026-05-08 15:16:08 +0800

    创建云机弹框内DNS和子网填充默认值

--- src/pages/vm/dev_list.tsx
+++ src/pages/vm/dev_list.tsx
@@ -378,6 +378,8 @@ export class DeviceList extends tsx.Component<IProps, IEvents> {
                 height: 1280,
                 dpi: 320,
                 fps: 24,
+                subnet: "10.93.51.0/24",
+                dns_urls: "223.5.5.5",
             },
         });
         if (re) this.$emit("changed", data.hostIp);
