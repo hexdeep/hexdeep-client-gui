c2e7db1d172ceb298ef7134bfd1aef6c1b8d555f
Sauce233 <2892709432@qq.com>
2026-07-17 10:10:27 +0800

    在云机列表中标注v3机型云机

--- src/pages/vm/dev_list.tsx
+++ src/pages/vm/dev_list.tsx
@@ -290,6 +290,17 @@ export class DeviceList extends tsx.Component<IProps, IEvents> {
             <div class={[s.listCell, s.colImage]}>
                 {!img ? <span>{row.image_addr}</span> : (
                     <div>
+                        {row.create_req?.mobile_model_version === "v3" && <el-tag
+                            size="mini"
+                            effect="dark"
+                            style={{
+                                marginRight: "5px",
+                                background: "linear-gradient(135deg, #36d1dc 0%, #5b86e5 100%)",
+                                border: "none",
+                                color: "#fff",
+                                fontWeight: "bold",
+                            }}
+                        >v3</el-tag>}
                         {img.android_version && <span
                             style={{
                                 lineHeight: "20px",
